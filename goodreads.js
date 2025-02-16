9af0ead2795a4a38192fc92922b4bae9f421c9de
Mahmoud <msamori@proton.me>
2025-02-16 15:14:11 -0500

    add container DIVs to mimic other buttons

--- goodreads.js
+++ goodreads.js
@@ -7,19 +7,41 @@ function addGoodreadsButton() {
     // Prevent duplicate buttons
     if (document.getElementById("nyplButton")) return;
 
-    // Create the button immediately
+    // Create button container
+    let buttonActions = document.createElement("div");
+    buttonActions.className = "BookActions__button";
+
+    let buttonGroup = document.createElement("div");
+    buttonGroup.className = "ButtonGroup ButtonGroup--block";
+
+    let buttonContainer = document.createElement("div");
+    buttonContainer.className = "Button__container Button__container--block";
+
+    // Create button
     let button = document.createElement("button");
     button.id = "nyplButton";
-    button.innerText = "NYPL";
-    button.className = "Button Button--buy Button--medium Button--block";
+    button.type = "button";
+    button.className = "Button Button--wtr Button--medium Button--block";
     button.disabled = false; 
     button.onclick = () => {
         let searchUrl = `https://borrow.nypl.org/search?query=${encodeURIComponent(title)}`;
         window.open(searchUrl, "_blank");
     };
 
+    // Create button label span
+    let buttonLabel = document.createElement("span");
+    buttonLabel.className = "Button__labelItem";
+    buttonLabel.innerText = "NYPL";
+
+    // Append elements 
+
+    button.appendChild(buttonLabel);
+    buttonContainer.appendChild(button);
+    buttonGroup.appendChild(buttonContainer);
+    buttonActions.appendChild(buttonGroup);
+
     // Insert the button right away
-    buttonDiv.insertBefore(button, shopButton);
+    buttonDiv.insertBefore(buttonActions, shopButton);
 }
 
 addGoodreadsButton();
