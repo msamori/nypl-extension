b866071ab6e583c41a095d1ccc543f2f445d7402
Mahmoud <msamori@proton.me>
2025-02-18 00:02:54 -0500

    set referrerPolicy = 'no-referrer' on the link

--- link.js
+++ link.js
@@ -4,7 +4,9 @@ function createLibraryLink(title) {
     link.href = `https://borrow.nypl.org/search?query=${encodeURIComponent(title)}`;
     link.target = "_blank";
     link.rel = "noopener noreferrer";
+    link.referrerPolicy = "no-referrer";
     link.innerText = "NYPL";
+    console.log("link: ", link);
 
     return link;
 }
